82fdf564718657a837332582185646915c656b1a
missimba <38391085+missimba@users.noreply.github.com>
2022-04-27 16:41:42 +0800

    Update LuaDLL.cs

--- Assets/XLua/Src/LuaDLL.cs
+++ Assets/XLua/Src/LuaDLL.cs
@@ -38,6 +38,15 @@ namespace XLua.LuaDLL
 #else
         const string LUADLL = "xlua";
 #endif
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        //对应pbc-lua53.c文件__declspec(dllexport) int luaopen_protobuf_c(lua_State *L)函数
+        public static extern int luaopen_protobuf_c(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadProtobuf(System.IntPtr L)
+        {
+            return luaopen_protobuf_c(L);
+        }
 
         [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr lua_tothread(IntPtr L, int index);
